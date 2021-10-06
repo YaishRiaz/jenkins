05db56f1e40c108daa2106c0437049d6684711df
Yaish Riaz <66737404+YaishRiaz@users.noreply.github.com>
2021-10-06 11:21:32 +0530

    Update Hello.java
    This is the change where we removed the command line input and are just going to run an output which says Hello World!

--- src/Hello.java
+++ src/Hello.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Hello{
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		String name;
-		int age;
+// 		Scanner scanner = new Scanner(System.in);
+// 		String name;
+// 		int age;
 		
-		System.out.print("Enter your name :");
-		name = scanner.next();
-		System.out.print("Enter your age  :");
-		age = scanner.nextInt();
-		System.out.println("Your name is "+ name + " and your age is " + age);
+// 		System.out.print("Enter your name :");
+// 		name = scanner.next();
+// 		System.out.print("Enter your age  :");
+// 		age = scanner.nextInt();
+// 		System.out.println("Your name is "+ name + " and your age is " + age);
+		
+		System.out.print("Hello World!");
 
 	}
 }
